59a94694645b30c5e8e418429f07899a82866aab
EhsanWebDev <58270856+EhsanWebDev@users.noreply.github.com>
2020-04-15 18:15:48 +0500

    Updates

---
+++ precache-manifest.79f667b6081f4833323021809abf79bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b77ca3524c1ddf47448843361fb7355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef5c88f7e5b4fb07647",
+    "url": "/static/css/main.3f0034a8.chunk.css"
+  },
+  {
+    "revision": "08b10a3f402eb2d1adf2",
+    "url": "/static/js/2.31441719.chunk.js"
+  },
+  {
+    "revision": "cef5c88f7e5b4fb07647",
+    "url": "/static/js/main.e994d5b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
